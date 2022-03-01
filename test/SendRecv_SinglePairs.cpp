1c460fe42521da6201c3d9eae16c7c53978e5e81
akolliasAMD <akollias@amd.com>
2022-03-01 02:12:56 +0000

    adding multiple numbers for size

---
+++ test/SendRecv_SinglePairs.cpp
@@ -0,0 +1,83 @@
+/*************************************************************************
+ * Copyright (c) 2022 Advanced Micro Devices, Inc. All rights reserved.
+ *
+ * See LICENSE.txt for license information
+ ************************************************************************/
+#include "TestBed.hpp"
+
+namespace RcclUnitTesting
+{
+  TEST(SendRecv, SinglePairs)
+  {
+    TestBed testBed;
+
+    // Configuration
+    std::vector<ncclDataType_t> const& dataTypes       = {ncclInt32, ncclFloat64};
+    std::vector<int>            const  numElements     = {1048576, 53327, 1024};
+    bool                        const  inPlace         = false;
+    bool                        const  useManagedMem   = false;
+    // SendRecv_AllPairs true or false?
+    int numCollPerGroup = 0;
+    bool isCorrect = true;
+    int totalRanks = testBed.ev.maxGpus;
+    int const numProcesses = 1;
+    int const isMultiProcess = 0; //multi process is the next step
+    testBed.InitComms(TestBed::GetDeviceIdsList(numProcesses, totalRanks), 0);
+
+    for (int dataIdx = 0; dataIdx < dataTypes.size() && isCorrect; ++dataIdx)
+    {
+      for (int numIdx = 0; numIdx < numElements.size() && isCorrect; ++numIdx)
+      {
+        // Run all element sizes in parallel as single group
+        for (int sendRank = 0; sendRank < totalRanks; ++sendRank)
+        {
+          for (int recvRank = 0; recvRank  < totalRanks; ++recvRank )
+          {
+            testBed.SetCollectiveArgs(ncclCollSend,
+                                      dataTypes[dataIdx],
+                                      ncclSum, // This should be moved to optional variables struct
+                                      recvRank ,
+                                      numElements[numIdx],
+                                      numElements[numIdx],
+                                      0,
+                                      sendRank);
+            if (recvRank == 0)
+            {
+
+              testBed.AllocateMem(inPlace, useManagedMem, -1, sendRank);
+              testBed.PrepareData(-1, sendRank);
+            }
+            if (recvRank  != sendRank)
+            {
+              if (testBed.ev.showNames) // Show test names
+                INFO("%s process Datatype: %s SendReceive test Rank %d -> Rank %d\n",
+                    isMultiProcess ? "Multi " : "Single",
+                    ncclDataTypeNames[dataTypes[dataIdx]],
+                    sendRank,
+                    recvRank );
+
+
+              testBed.SetCollectiveArgs(ncclCollRecv,
+                                        dataTypes[dataIdx],
+                                        ncclSum, // This should be moved to optional variables struct
+                                        sendRank,
+                                        numElements[numIdx],
+                                        numElements[numIdx],
+                                        0,
+                                        recvRank );
+              testBed.AllocateMem(inPlace, useManagedMem, -1, recvRank );
+              testBed.PrepareData(-1, recvRank );
+              testBed.ExecuteCollectives({sendRank,recvRank });
+              testBed.ValidateResults(isCorrect, -1, recvRank );
+              testBed.DeallocateMem(-1, recvRank );
+            }
+
+          }
+          testBed.DeallocateMem(-1, sendRank);
+        }
+      }
+    }
+    testBed.DestroyComms();
+    testBed.Finalize();
+  }
+}
