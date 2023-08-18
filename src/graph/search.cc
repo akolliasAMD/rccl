d678f255a2773cdd2cece3b643b7dc8f30f91244
akolliasAMD <akollias@amd.com>
2023-08-17 23:53:58 -0400

    cleanup

--- src/graph/search.cc
+++ src/graph/search.cc
@@ -887,13 +887,20 @@ ncclResult_t ncclTopoCompute(ncclTopoSystem* system, struct ncclTopoGraph* graph
   }
 
   str = getenv("NCCL_RINGS");
-  if (str) {
+  char* strTrees = getenv("NCCL_TREES");
+
+  if (str || strTrees) {
     // user supplied topo
-    NCCLCHECK(parseGraph(str, system, graph, NULL, NULL));
-    int arch, vendor, model;
-    NCCLCHECK(ncclTopoCpuType(system, &arch, &vendor, &model));
-    if (graph->nChannels && arch == NCCL_TOPO_CPU_ARCH_X86 && vendor == NCCL_TOPO_CPU_VENDOR_AMD && model == NCCL_TOPO_CPU_TYPE_ROME) {
-      system->type |= RCCL_TOPO_4P2H_ROME;
+    if (strTrees) {
+      NCCLCHECK(parseGraphLight(strTrees, system, graph, NULL));
+      system->treeDefined=true;
+    } else {
+      NCCLCHECK(parseGraph(str, system, graph, NULL, NULL));
+      int arch, vendor, model;
+      NCCLCHECK(ncclTopoCpuType(system, &arch, &vendor, &model));
+      if (graph->nChannels && arch == NCCL_TOPO_CPU_ARCH_X86 && vendor == NCCL_TOPO_CPU_VENDOR_AMD && model == NCCL_TOPO_CPU_TYPE_ROME) {
+        system->type |= RCCL_TOPO_4P2H_ROME;
+      }
     }
   } else if (!rcclParamModelMatchingDisable() && !graph->collNet) {
     // try to match 8P6L
@@ -908,12 +915,7 @@ ncclResult_t ncclTopoCompute(ncclTopoSystem* system, struct ncclTopoGraph* graph
     // try to match 4H4P
     NCCLCHECK(parse4H4P(system, graph));
   }
-  char* strTrees = getenv("NCCL_TREES");
-  if (strTrees) {
-    // user supplied topo
-    NCCLCHECK(parseGraphLight(strTrees, system, graph, NULL));
-    system->treeDefined=true;
-  }
+
   if (graph->nChannels) return ncclSuccess;
 
   if ((graph->pattern == NCCL_TOPO_PATTERN_RING) && (system->type & RCCL_TOPO_4P2H_ROME) && (ngpus == system->nRanks)) {
